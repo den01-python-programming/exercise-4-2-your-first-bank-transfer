2b1430ebb97bc30ada06c8e08f6528139d025243
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 11:53:00 +0100

    Update account.py

--- src/account.py
+++ src/account.py
@@ -1,6 +1,19 @@
 class Account:
 
-    def __init__(self):
+    def __init__(self,name,balance):
         # initialise variables here
-        
+        self.name = name
+        self.balance = balance
+
     # write methods here
+    def balance(self):
+        return self.balance
+
+    def deposit(self,amount):
+        self.balance += amount
+
+    def withdraw(self,amount):
+        self.balance -= amount
+
+    def __str__(self):
+        return self.name + ", balance: " + str(self.balance)
